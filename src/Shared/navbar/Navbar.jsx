e7a96143ff5bbb0e225615b93ac56e34d25c2f78
Nazmul-Hassan-10 <adornazmulhassan@gmail.com>
2023-11-04 22:55:43 +0600

    error fixing

--- src/Shared/navbar/Navbar.jsx
+++ src/Shared/navbar/Navbar.jsx
@@ -55,7 +55,7 @@ const Navbar =() => {
                             textDecoration: 'none',
                         }}
                     >
-                        LOGO
+                        LOG
                     </Typography>
 
                     <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
